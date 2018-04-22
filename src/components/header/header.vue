56c34578b24c38101f8d9c9ffaa3d0d847c5c873
Abey Joy <abey.joy@nyu.edu>
2018-04-22 13:59:38 -0400

    header work, added FA and bootstrap

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    test
+  <div class="aj-header-main navbar navbar-inverse">
+    <h1> Abey Joy </h1>
+    <div class="aj-social-icons">
+      <i class="fab fa-github"></i>
+      <i class="fas fa-camera-retro"></i>
+      <i class="fab fa-facebook-f"></i>
+    </div>
+
   </div>
 </template>
 
@@ -13,4 +19,36 @@ export default {
 </script>
 
 <style lang="scss">
+  .aj-header-main {
+    // background: #000;
+    h1 {
+      padding: 15px;
+      // color: #ef0d33;
+      // color: white;
+      display: inline-block;
+
+    }
+    .aj-social-icons {
+      display: inline-block;
+      position: absolute;
+      right: 10px;
+      top: 10px;
+      i {
+        padding: 5px;
+      }
+    }
+    .fa-facebook-f {
+      font-size: 2.6em;
+      color: #3b5998;
+    }
+    .fa-github {
+      font-size: 3em;
+      color: grey;
+    }
+    .fa-camera-retro {
+      display: inline-block;
+      font-size: 3em;
+      color: brown;
+    }
+  }
 </style>
