9508a5aa6b845a01f597db6673e0f799a127792d
Abey Joy <abey.joy@nyu.edu>
2018-04-25 00:53:28 -0400

    Skill Set section

--- src/components/skills/skills.vue
+++ src/components/skills/skills.vue
@@ -19,7 +19,7 @@
     <div class="card col-xs-12 col-s-12 col-md-3 col-lg-3 col-xl-3">
       <img src="https://firebasestorage.googleapis.com/v0/b/abeyjoysite.appspot.com/o/skill-programming.gif?alt=media&token=cb133233-e052-43a9-8e64-dd70807dec16" alt="">
       <div class="card-body">
-        <h5 class="card-title">Programming Skills</h5>
+        <h5 class="card-title">Programming</h5>
         <span class="skills-programming-section">
           <div>
             <img class="prg-img" src="https://firebasestorage.googleapis.com/v0/b/abeyjoysite.appspot.com/o/jsicon.png?alt=media&token=78b0e10e-8990-407c-b04d-6e49b59e6956">
@@ -49,14 +49,21 @@
     <div class="card col-xs-12 col-s-12 col-md-3 col-lg-3 col-xl-3">
       <img src="https://firebasestorage.googleapis.com/v0/b/abeyjoysite.appspot.com/o/skill-interest.gif?alt=media&token=40c16eb3-524d-4312-b115-e420e9ef856c" alt="">
       <div class="card-body">
-        <h5 class="card-title">Interests</h5>
+        <h5 class="card-title">Skill Set</h5>
+        <ul class="skill-list">
+          <li> Product Design </li>
+          <li> Test Driven Development</li>
+          <li> Automation </li>
+          <li> Building out Engineering/Product Teams</li>
+          <li> Data Visualization</li>
+        </ul>
       </div>
     </div>
 
     <div class="card col-xs-12 col-s-12 col-md-3 col-lg-3 col-xl-3">
       <img src="https://firebasestorage.googleapis.com/v0/b/abeyjoysite.appspot.com/o/skills-trivia-game.gif?alt=media&token=79df161c-642b-4fe4-8386-22665e7aeb2a" alt="">
       <div class="card-body">
-        <h5 class="card-title">Trivia</h5>
+        <h5 class="card-title">Interests</h5>
       </div>
     </div>
 
@@ -110,4 +117,7 @@
     }
   }
 
+  .skill-list {
+    padding: 0 15px;
+  }
 </style>
